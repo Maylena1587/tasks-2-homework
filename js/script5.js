35b64550cafba1cff0233c084ddd9961c30ba765
Maylena Lorentes <lorentes@ukr.net>
2020-08-02 23:48:24 +0300

    вносит правки

--- js/script5.js
+++ js/script5.js
@@ -1,3 +1,9 @@
+// Задача 2 - 5
+// Функция предикат
+// Функция предикат возвращает true или false
+
+// Напиши функцию checkForSpam(message), принимающую 1 параметр message - строку.Функция проверяет ее на содержание слов spam и sale.Если нашли запрещенное слово то функция возвращает true, если запрещенных слов нет функция возвращает false.Слова в строке могут быть в произвольном регистре.
+
 function checkForSpam(str) {
   const message = str.toLowerCase();
   const isThiseWord = message.includes("sale") || message.includes("spam");
--- js/script7.js
+++ js/script7.js
@@ -3,10 +3,11 @@
 // Напиши функцию filterArray(array), принимающую 1 параметр array - массив.Функция проверяет каждое значение массива - является ли значение конечным числом(Number.isFinite).Для проверки используй цикл for.Числа добавляются в новый массив numbers с помощью метода push, а не числа - игнорируются.По завершению проверки массива array возвращается массив numbers.
 function filterArray(array) {
   const numbers = [];
+
   for (let i = 0; i < array.length; i += 1) {
-    // Write code under this line
-    numbers.push(array[i]);
-    Number.isNaN(array[i]);
+    if (Number.isFinite(array[i])) {
+      numbers.push(array[i]);
+    }
   }
   return numbers;
 }
